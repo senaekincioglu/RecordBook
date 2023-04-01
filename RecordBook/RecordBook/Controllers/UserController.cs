ce3da5cdfaa6b828c246753a615c5804d49548b6
senaekincioglu <115897687+senaekincioglu@users.noreply.github.com>
2023-04-02 00:39:22 +0300

    User girişi controller a ait giriş işlemleri için get ve post olmak üzere backend kodları yazıldı

--- RecordBook/RecordBook/Controllers/UserController.cs
+++ RecordBook/RecordBook/Controllers/UserController.cs
@@ -1,17 +1,35 @@
-﻿using System;
+﻿using RecordBook.Models.Entity;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using RecordBook.Models;
 
 namespace RecordBook.Controllers
 {
     public class UserController : Controller
     {
         // GET: User
-        public ActionResult Index()
+        RecordBookEntities db = new RecordBookEntities();
+        public ActionResult Login()
         {
             return View();
+        }
+        [HttpPost]
+        public ActionResult Login(User user)
+        {
+            var informations = db.User.FirstOrDefault(x => x.Email == user.Email && x.Password == user.Password);
+            if (informations != null) //gelen değerler boş değilse
+            {
+                return RedirectToAction("Index", "Admin");
+            }
+            else
+            {
+                return RedirectToAction("Login", "User");
+
+            }
+
         }
     }
 }
